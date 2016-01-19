d057c828ce03b1d708a10eb480d3c0caeb9b1412
Bui Sy Nguyen <nguyenbs@projectkit.net>
2016-01-19 18:16:42 +0700

    Improve code for Deserializer

--- tests/Zend/Amf/WorkCalendar.php
+++ tests/Zend/Amf/WorkCalendar.php
@@ -3,7 +3,17 @@ class WorkCalendar
 {
     public $id;
     public $name;
+    /**
+     * @var array $periods The time periods
+     * @serialize Vector<net.fproject.calendar.Period>
+     * */
     public $periods;
+
+    /**
+     * @var array $periods The time periods
+     * @amftype Vector<net.fproject.calendar.WeekDay>
+     * */
     public $weekDays;
+
     public $baseCalendar;
 }
